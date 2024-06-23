a080e132b0c9f3b60b465bd82c41331ccba0a48e
Kfir-G <kfirg15@gmail.com>
2024-06-23 13:21:21 +0200

    setup postgress

---
+++ db_commands.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+psql -h 0.0.0.0 -U ${POSTGRES_USER} -d ${POSTGRES_DB}
--- web_command.sh
+++ web_command.sh
@@ -6,3 +6,6 @@ npm install nodemon --save-dev
 npm install sequelize --save
 npm install sequelize-cli --save
 npx sequelize-cli init
+npm install pg --save
+npx sequelize-cli model:generate --name User --attributes email:string
+npx sequelize-cli db:migarte 
