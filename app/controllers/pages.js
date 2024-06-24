0bb1a86258853f50f40071b145ed530bf3c8dea6
Kfir-G <kfirg15@gmail.com>
2024-06-24 16:30:19 +0200

    set up routes and controllers

---
+++ app/controllers/pages.js
@@ -0,0 +1,9 @@
+class PagesController {
+    welcome(req, res, next) {
+      if (!req.user) return res.send("Hello Universe!");
+      next();
+    }
+  }
+  
+  module.exports = PagesController;
+  
